4536d868dcf59a3b1a219424738d7e428e6a59eb
nighmared <jonas.konrath@icloud.com>
2026-06-16 21:02:43 +0200

    bump

--- fish.fish
+++ fish.fish
@@ -20,6 +20,8 @@ set -gx PATH $PATH $HOME/intel/oneapi/advisor/latest/bin64
 set -gx PATH $PATH $HOME/.local/share/flutter/bin
 set -gx PATH $PATH $HOME/Android/Sdk/cmdline-tools/latest/bin
 
+set -gx PATH $PATH /opt/gradle/gradle-9.3.1/bin
+
 #gopath
 set -x GOPATH $HOME/go
 #DONT -U with set aaa
@@ -28,16 +30,23 @@ set -x CHROME_EXECUTABLE google-chrome
 
 alias getvenv="source .venv/bin/activate.fish"
 
-set -x THESIS_PATH $HOME/Documents/ETH/master_thesis/cluster_test/utils
-alias thesisutil="$THESIS_PATH/cli/cli"
+set -x MICROSERVICE_UTIL_PATH $HOME/Documents/ETH/master_thesis/cluster_test/utils
+alias thesisutil="$MICROSERVICE_UTIL_PATH/cli/cli"
 alias tu=thesisutil
+#alias parser="$MICROSERVICE_UTIL_PATH/parser_cli.py"
+alias kibanatunnel="ssh -L 5601:localhost:1234 slimfly12 kubectl port-forward -n thesis services/quickstart-kb-http 1234:5601"
+alias elastictunnel="ssh -L 9200:localhost:9200 slimfly12 kubectl port-forward -n thesis svc/quickstart-es-http 9200"
+alias statstunnel="ssh -L 9000:localhost:9000 slimfly8 kubectl port-forward -n knative-serving deploy/3scale-kourier-gateway 9000"
+
+alias resettunnels="ssh slimfly12 killall kubectl"
 
 #LUA DEPENDENCY HELL OMG
 eval "$(luarocks --lua-version 5.1 path)"
-
+set -x LUA_PATH "$LUA_PATH;$HOME/Documents/projects/thrift/lib/lua/?.lua"
+set -x LUA_CPATH "$LUA_CPATH;$HOME/Documents/projects/thrift/lib/lua/src/?.so"
 alias getsecret="jq '.data.value' -r | base64 --decode | cut -c 1-"
 
-alias theme="npx alacritty-themes"
+#alias theme="npx alacritty-themes"
 
 #homeserver
 set -x defaultbrowser xdg-settings get default-web-browser | cut -d "." -f 1
