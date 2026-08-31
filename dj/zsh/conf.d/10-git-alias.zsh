6eecd7616da50e07c60f6fbba92de77b1a2e3d7f
Jonas K <jonas.konrath@deepjudge.ai>
2026-08-31 15:34:28 +0200

    :)

--- dj/zsh/conf.d/10-git-alias.zsh
+++ dj/zsh/conf.d/10-git-alias.zsh
@@ -1,2 +1,3 @@
 alias gp="git pull"
 alias gs="git status"
+alias gpr='git push --set-upstream origin "$(git branch --show-current --quiet)"; gh pr create -f'
--- dj/zsh/conf.d/20-dj-aliases.zsh
+++ dj/zsh/conf.d/20-dj-aliases.zsh
@@ -1,2 +1,5 @@
 alias dj2venv="source $DJ2_DIR/.venv/bin/activate"
 alias run_local="dj2venv && cd $DJ2_DIR && tilt up --context docker-desktop -- --config-path $DJ2_DIR/config/dev-tilt.local.jsonnet && deactivate"
+
+alias argoresume="kubectl scale statefulset -n argocd argocd-application-controller --replicas 1"
+alias argopause="kubectl scale statefulset -n argocd argocd-application-controller --replicas 0"
